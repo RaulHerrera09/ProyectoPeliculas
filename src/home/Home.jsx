73a724ada20e350cff900ee4cade184a3bbb57d9
@RaulHerrera09 <raulherreradelgadillo09@gmail.com>
2023-09-08 12:56:24 -0600

    ProyectoPeliss

--- src/home/Home.jsx
+++ src/home/Home.jsx
@@ -1,8 +1,12 @@
-// eslint-disable-next-line no-unused-vars
 import React from 'react'
+import NavComponet from '../nav/Nav.jsx';
+import { Outlet } from "react-router-dom";
 
 export default function Home() {
     return (
-        <div>Home</div>
+        <>
+            <NavComponet />
+            <Outlet />
+        </>
     )
 }
